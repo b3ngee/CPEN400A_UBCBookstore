253081c1eb710afc8e4900b5dba2ecc03b2a9167
b3ngee <bg.92@live.com>
2017-10-17 20:53:12 -0700

    Fixed names
    fixed names
    
    fixed png

--- js/ShoppingCart.js
+++ js/ShoppingCart.js
@@ -2,18 +2,28 @@
 var cart = {};
 
 var products = {
-    Box1: 5,
-    Box2: 5,
-    Clothes1: 5,
-    Clothes2: 5,
-    Jeans: 5,
-    KeyboardCombo: 5,
-    Keyboard: 5,
-    Mice: 5,
-    PC1: 5,
-    PC2: 5,
-    PC3: 5,
-    Tent: 5
+    'Box1': { product: new Product('Box1', 10, './Images/Products/Box1_$10.png'), quantity: 5 },
+    'Box2': { product: new Product('Box2', 5, './Images/Products/Box2_$5.png'), quantity: 5 },
+    'Clothes1': { product: new Product('Clothes1', 20, './Images/Products/Clothes1_$20.png'), quantity: 5 },
+    'Clothes2': { product: new Product('Clothes2', 30, './Images/Products/Clothes2_$30.png'), quantity: 5 },
+    'Jeans': { product: new Product('Jeans', 50, './Images/Products/Jeans_$50.png'), quantity: 5 },
+    'KeyboardCombo': { product: new Product('KeyboardCombo', 40, './Images/Products/KeyboardCombo_$40.png'), quantity: 5 },
+    'Keyboard': { product: new Product('Keyboard', 20, './Images/Products/Keyboard_$20.png'), quantity: 5 },
+    'Mice': { product: new Product('Mice', 20, './Images/Products/Mice_$20.png'), quantity: 5 },
+    'PC1': { product: new Product('PC1', 350, './Images/Products/PC1_$350.png'), quantity: 5 },
+    'PC2': { product: new Product('PC2', 400, './Images/Products/PC2_$400.png'), quantity: 5 },
+    'PC3': { product: new Product('PC3', 300, './Images/Products/PC3_$300.png'), quantity: 5 },
+    'Tent': { product: new Product('Tent', 100, './Images/Products/Tent_$100.png'), quantity: 5 }
+}
+
+function Product(name, price, imageUrl) {
+    this.name = name;
+    this.price = price;
+    this.imageUrl = imageUrl;
+}
+
+Product.prototype.computeNetPrice = function(quantity) {
+    return this.price * quantity;
 }
 
 var inactiveTime = 0;
